3f88fbccd57f7d03ec658d7f6f7792f85aa02dbe
Musa Abdillah <musa@clappingape.com>
2020-04-26 21:36:55 +0700

    initialize message-board

---
+++ components/footer.jsx
@@ -0,0 +1,16 @@
+const React = require("react")
+
+const Footer = (props) => {
+	return(
+		<div>
+			<hr/>
+			<div className="row-fluid">
+				<div className="span12">
+				<p>2020 musa writing with &heart;</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+module.exports = Footer
---
+++ components/header.jsx
@@ -0,0 +1,13 @@
+const React = require("react")
+
+const Header = () => {
+	return(
+		<div>
+			<div className="span12">
+				<p>Message Board</p>
+			</div>
+		</div>
+	)
+}
+
+module.exports = Header
